357891071db2c41d3f47602212410f8e571db425
Jhonatan Tamayo Morales <Jhonatan.Tamayo@endava.com>
2024-10-20 17:15:28 -0500

    Created employee portal main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ShopListComponent } from './components/shop-list/shop-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { HomeComponent } from './components/home/home.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { EmployeePortalComponent } from './components/employee-portal/employee-portal.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
@@ -13,7 +14,8 @@ export const routes: Routes = [
     {path: 'forgot-password',component: AuthLayoutComponent, data: {formType: 'forgot'}},
     {path: 'products',component:ShopListComponent},
     { path: 'products/:id', component: ProductDetailComponent }, 
-    {path: 'wishlist',component: WishlistComponent}
+    {path: 'wishlist',component: WishlistComponent},
+    {path: 'employee-portal', component: EmployeePortalComponent}
 ];
 
 @NgModule({
